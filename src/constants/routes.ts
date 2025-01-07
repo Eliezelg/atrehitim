a93d7cd50b60bbb5ed0ab5bbe295dfe638f09c8f
Eliezelg <gazettefamily@gmail.com>
2025-01-07 02:16:04 +0200

    Delete src directory

--- src/constants/routes.ts
+++
@@ -1,9 +0,0 @@
-export const ROUTES = {
-  home: '/',
-  about: '/about',
-  shipping: '/shipping',
-  terms: '/terms',
-  category: (slug: string) => `/category/${slug}`,
-  subcategory: (category: string, subcategory: string) => `/category/${category}/${subcategory}`,
-  product: (id: string) => `/product/${id}`
-};
